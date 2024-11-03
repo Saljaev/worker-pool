6462116b999be3c40867bcfccc15619e067a17a9
Konstantin Salyaev <r.cf2003@yandex.ru>
2024-11-03 17:59:53 +0300

    feat(main): add worker-pool controller from console

---
+++ cmd/main.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"bufio"
+	_ "embed"
+	"errors"
+	"flag"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+	wpool "worker-pool/workers"
+)
+
+//go:embed help.txt
+var help string
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	workerCount := flag.Int("count", 3, "Initial count of workers")
+	separate := flag.String("sep", ",", "Separate of jobs")
+
+	flag.Parse()
+
+	wp := wpool.NewWorkerPool(*workerCount)
+
+	workerControl()
+
+	go wp.Run()
+
+	for scanner.Scan() {
+		var data = scanner.Text()
+
+		str := strings.Split(data, " ")
+
+		switch str[0] {
+		case "/add":
+			if len(str) <= 1 {
+				fmt.Println("input zero count")
+				break
+			}
+			count, err := strconv.Atoi(str[1])
+			if err != nil {
+				fmt.Printf("ivalid count of workers: %v", err)
+				break
+			}
+			err = wp.ChangeWorkersLimit(count)
+			if errors.Is(err, wpool.ErrInvalidDelta) {
+				fmt.Printf("invalid %d", count)
+				break
+			} else {
+				fmt.Printf("Limit has set to %d\n", wp.GetWorkersLimit())
+			}
+		case "/remove":
+			if len(str) <= 1 {
+				fmt.Println("input zero count")
+				break
+			}
+			count, err := strconv.Atoi(str[1])
+			if err != nil {
+				fmt.Printf("ivalid count of workers: %v", err)
+				break
+			}
+			err = wp.ChangeWorkersLimit(-count)
+			if errors.Is(err, wpool.ErrInvalidDelta) {
+				fmt.Printf("invalid %d", count)
+				break
+			} else {
+				fmt.Printf("Limit has set to %d\n", wp.GetWorkersLimit())
+			}
+		case "/limit":
+			fmt.Println("Count of workers", wp.GetWorkersLimit())
+		case "/help":
+			workerControl()
+		case "/addjob":
+			if len(str) <= 1 {
+				fmt.Println("input zero jobs")
+				break
+			}
+			for _, job := range str[1:] {
+				for _, payload := range strings.Split(job, *separate) {
+					wp.AddPayloadItem(payload)
+				}
+			}
+		}
+	}
+}
+
+func workerControl() {
+	fmt.Println(help)
+}
