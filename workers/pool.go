424df4d3ae4591909132e02ec4a77f4872012735
Konstantin Salyaev <r.cf2003@yandex.ru>
2024-11-03 17:13:01 +0300

    feat(pool): add worker-pool

---
+++ workers/pool.go
@@ -0,0 +1,52 @@
+package workers
+
+import (
+	"errors"
+	"github.com/google/uuid"
+)
+
+type WorkerPool struct {
+	limiter chan struct{}
+	payload chan interface{}
+}
+
+func NewWorkerPool(limit int) *WorkerPool {
+	return &WorkerPool{
+		limiter: make(chan struct{}, limit),
+		payload: make(chan interface{}),
+	}
+}
+
+// ChangeWorkersLimit change count of workers by delta
+func (wp *WorkerPool) ChangeWorkersLimit(delta int) error {
+	limit := cap(wp.limiter) + delta
+	if limit < 0 {
+		return ErrInvalidDelta
+	}
+
+	wp.limiter = make(chan struct{}, limit)
+	return nil
+}
+
+func (wp *WorkerPool) AddPayloadItem(payloadItem interface{}) {
+	wp.payload <- payloadItem
+}
+
+func (wp *WorkerPool) GetWorkersLimit() int {
+	return cap(wp.limiter)
+}
+
+// Run start WorkerPool
+func (wp *WorkerPool) Run() {
+	for {
+		payloadItem := <-wp.payload
+
+		wp.limiter <- struct{}{}
+
+		go do(uuid.New(), payloadItem, func() {
+			<-wp.limiter
+		})
+	}
+}
+
+var ErrInvalidDelta = errors.New("invalid delta")
